6642869c51f6924f2e842b5788319ba1773c100a
nickpapadakis-matc <papadakn@gmatc.matc.edu>
2018-02-19 10:58:56 -0600

    to see if my c# apps commit to repo

---
+++ WorkWithGitApp/WorkWithGitApp/GitIt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithGitApp
+{
+    class GitIt
+    {
+        public GitIt()
+        {
+
+        }
+        public void GitSomething()
+        {
+            Console.WriteLine("This is to make sure my repo is set up!");
+        }
+    }
+}
---
+++ WorkWithGitApp/WorkWithGitApp/WorkWIthGitAPP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithGitApp
+{
+    class WorkWIthGitAPP
+    {
+        static void Main(string[] args)
+        {
+
+            GitIt g = new GitIt();
+            g.GitSomething();
+            Console.ReadLine();
+
+        }
+    }
+}
